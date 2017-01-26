4b80ee80aa4642da76f9d62e1ac9d70aaefe1883
StevenSchoening <StevenSchoening@gmx.de>
2017-01-27 00:54:55 +0100

    sql

--- _functions/articles/createArticle.php
+++ _functions/articles/createArticle.php
@@ -10,16 +10,21 @@ require_once "../_init.php";
 function createArticle(){
 
     $database = Database::getLastInstance();
+   // $database = new Database;
+   // $database->select_db("nuuws");
 
     //todo add user id value
     $title = $database->real_escape($_POST['title']);
     $content = $database->real_escape($_POST['content']);
+    $sql = "INSERT INTO `nuuws.news`(`newsID`, `title`, `content`, `createdTS`, `userID`, `published`, `crawlerURI`) VALUES (NULL,'$title','$content', NULL, '0','0', '')";
 
-    $database->query("INSERT INTO `news`(`newsID`, `title`, `content`, `createdTS`, `userID`, `published`, `crawlerURI`) VALUES (NULL,'$title','$content', NULL, NULL,'0', NULL)");
-   // ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7])
+    $database->query($sql);
 
-
-
+    if($database->query($sql) === true){
+        echo "data was sent successfully!";
+    }else{
+        echo "somehow you got an unexpected error...but we are surely already working on it";
+    }
 
 }
 
