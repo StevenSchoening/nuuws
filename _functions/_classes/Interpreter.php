eee711e4cb3080f3d5507c0a94e5aca9dfb0d427
mmuxi <mmuxi@gmx.de>
2017-02-14 12:57:00 -0800

    Kommentare

--- _functions/_classes/Interpreter.php
+++ _functions/_classes/Interpreter.php
@@ -14,6 +14,12 @@ abstract class Interpreter implements IInterpreter
 
     const imageRoot = 'C:/xampp/htdocs/nuuws/portal/assets/images/';
 
+    /**
+     * Interpreter constructor.
+     *
+     * @param $uri string unique request identifier
+     * @param $uriId string id from crawler Table
+     */
     public function __construct($uri, $uriId)
     {
         $this->isArticle       = FALSE;
@@ -24,6 +30,13 @@ abstract class Interpreter implements IInterpreter
         $this->headerImageInfo = $this->tags = [];
     }
 
+    /**
+     * transforms an array into a MySql conform string
+     *
+     * @param $arr
+     *
+     * @return string 'val1', 'val2', 'val3'
+     */
     protected function valueArrToMySqlString($arr)
     {
         $str = '';
@@ -35,7 +48,11 @@ abstract class Interpreter implements IInterpreter
         return substr($str, 0, -2);
     }
 
-
+    /**
+     * @param $uri
+     *
+     * @return string publisher / host
+     */
     public static function getPublisher($uri)
     {
         // parse url to retrieve host
@@ -46,11 +63,21 @@ abstract class Interpreter implements IInterpreter
         return strtolower($host[sizeof($host) - 2]); // -1 = top level domain, -2 = domain
     }
 
+    /**
+     * Crawler database entry will be set to `interpreted` = 1
+     *
+     * The interpreter will only not interpret it twice
+     *
+     * @return string MySql query
+     */
     private function getUpdateUriQuery()
     {
         return "UPDATE `crawleruri` SET `interpreted` = '1' WHERE `uriID` LIKE '{$this->id}'";
     }
 
+    /**
+     * results will be stored in the database automatically
+     */
     public function __destruct()
     {
         Database::getLastInstance()->query($this->getUpdateUriQuery());
@@ -67,6 +94,11 @@ abstract class Interpreter implements IInterpreter
         }
     }
 
+    /**
+     * @param $title string
+     *
+     * @return mixed trimmed $title
+     */
     public static function trimTitle($title)
     {
         $search  = [
