85a3ad52394b6b96904674f9a6ecafad1e29eeaf
nikja1 <nikja91@gmail.com>
2017-02-15 08:33:54 +0100

    Update SQL_News.sql
    Neue Tabelle newsimage
    Spaltenname `user` als `userID` ändern. Tabellenname ist News.

--- spezifikation/SQL_News.sql
+++ spezifikation/SQL_News.sql
@@ -95,6 +95,9 @@ CREATE TABLE `news` (
   `crawlerURI` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+ALTER TABLE `news` CHANGE `user` `userID` INT(10) NOT NULL;
+
 --
 -- Daten für Tabelle `news`
 --
@@ -287,3 +290,54 @@ ALTER TABLE `usergroup`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 15. Feb 2017 um 08:25
+-- Server-Version: 10.1.13-MariaDB
+-- PHP-Version: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `nuuws`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `newsimage`
+--
+
+CREATE TABLE `newsimage` (
+  `images` int(10) NOT NULL,
+  `news` int(10) NOT NULL,
+  `priority` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `newsimage`
+--
+ALTER TABLE `newsimage`
+  ADD PRIMARY KEY (`images`,`news`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
